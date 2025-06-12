782d5071fd1ef9d4e79d1a4f6b369e9f1cea2ea4
Trip7274 <github@trps.dev>
2025-06-12 20:34:37 +0300

    Improve code readability for the SemverScheme test

--- Bayt_API.Testing/GlobalsTests.cs
+++ Bayt_API.Testing/GlobalsTests.cs
@@ -7,7 +7,9 @@ public class GlobalsTests(ITestOutputHelper testOutputHelper)
 	[Fact]
 	public void SemverScheme()
 	{
-		testOutputHelper.WriteLine($"API Version: {ApiConfig.ApiVersion}\nVersion: {ApiConfig.Version}\nProcessed Characters: {ApiConfig.ApiVersion} == {ApiConfig.Version[..ApiConfig.Version.IndexOf('.')]}");
+		testOutputHelper.WriteLine($"API Version: {ApiConfig.ApiVersion}\n" +
+		                           $"Version: {ApiConfig.Version}\n" +
+		                           $"Processed Characters: {ApiConfig.ApiVersion} == {ApiConfig.Version[..ApiConfig.Version.IndexOf('.')]}");
 		Assert.Equal(ApiConfig.ApiVersion, byte.Parse(ApiConfig.Version[..ApiConfig.Version.IndexOf('.')]));
 	}
 }

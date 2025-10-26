14c52c78dd8389fbe7bbf274f84bb514e27ab8bb
Trip7274 <github@trps.dev>
2025-10-26 08:16:56 +0300

    Improve readability of LoggingTests.cs

--- Bayt_API.Testing/LoggingTests.cs
+++ Bayt_API.Testing/LoggingTests.cs
@@ -7,13 +7,12 @@ public class LoggingTests
 		5, 15, 0, 255, 7, 234, 206, 92, 40, 202, 107, 84, 101, 115, 116, 105, 110, 103, 2
 	];
 	private static readonly byte[] ReferenceContent = "This is a test!"u8.ToArray();
+	private static byte[] FullReferenceEntry => ReferenceHeader.Concat(ReferenceContent).ToArray();
 
 	[Fact]
 	public void TestParsingFull()
 	{
-		byte[] referenceArray = ReferenceHeader.Concat(ReferenceContent).ToArray();
-
-		var entry = LogEntry.Parse(referenceArray);
+		var entry = LogEntry.Parse(FullReferenceEntry);
 
 		Assert.Equal(StreamId.Info, entry.StreamId);
 		Assert.Equal("Testing", entry.ModuleName);
@@ -35,7 +34,9 @@ public class LoggingTests
 	{
 		var serializedEntry = new LogEntry(StreamId.Info, "Testing", "This is a test!", DateTime.MaxValue).Serialize();
 
+		if (FullReferenceEntry.Equals(serializedEntry)) return;
 
+		// This is just to split which part is incorrect.
 		Assert.Equal(ReferenceHeader, serializedEntry.AsSpan(0, ReferenceHeader.Length).ToArray());
 		Assert.Equal(ReferenceContent, serializedEntry.AsSpan(ReferenceHeader.Length).ToArray());
 	}
@@ -45,7 +46,9 @@ public class LoggingTests
 	{
 		var serializedEntry = new LogEntry(StreamId.Info, "\u0002T\res\nt\fi\tn\bg\v", "\n\t\r\u0002This i\fs a\v te\bst!\a", DateTime.MaxValue).Serialize();
 
+		if (FullReferenceEntry.Equals(serializedEntry)) return;
 
+		// This is just to split which part is incorrect.
 		Assert.Equal(ReferenceHeader, serializedEntry.AsSpan(0, ReferenceHeader.Length).ToArray());
 		Assert.Equal(ReferenceContent, serializedEntry.AsSpan(ReferenceHeader.Length).ToArray());
 	}

e7ec00569dd86893d5244989747fcb413442f3da
Trip7274 <github@trps.dev>
2026-03-07 08:30:33 +0300

    Clarify script's purpose more accurately in starting prompt

--- Bayt_API/CleanupBayt.sh
+++ Bayt_API/CleanupBayt.sh
@@ -27,7 +27,7 @@ logHelper() {
 	printf "$PREFIX %s\e[0m\n" "$1"
 }
 
-printf "We're sorry to see you go! If you're having any issues, do feel free to report them in our GitHub repo! (https://github.com/Trip7274/Bayt_API)\nThis script will reverse everything that \e[3;36mSetupBayt.sh\e[0m set up.\n\n"
+printf "We're sorry to see you go! If you're having any issues, do feel free to report them in our GitHub repo! (https://github.com/Trip7274/Bayt_API)\nThis script will reverse everything that \e[3;36mSetupBayt.sh\e[0m set up, and delete any Bayt-related files.\n\n"
 if [ "$SETUP_NOCONFIRM" != "1" ]; then
     printf "Most of this process is \e[0;33muser-specific\e[0m. This means that '\e[0;32m%s\e[0m' may not be the only user with permissions setup.\nTo check for all users, make sure the directory '/etc/sudoers.d/' is cleared of any files \e[4;37mstarting with the characters '10-BaytApi-[user]'\e[0m.\nPress enter to continue, or Ctrl+C to cancel\n" "$USER"
 	read -r _
